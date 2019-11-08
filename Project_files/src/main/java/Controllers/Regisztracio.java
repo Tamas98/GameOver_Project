9f274069a50b29d9da76d4d7e2838eee2eab7171
TothCsaba <99letter86@gmail.com>
2019-11-08 22:49:03 +0100

    Üdvözlő illetve a Regisztráció ablak bezárása ha az egyik 'megnyílik'

--- Project_files/src/main/java/Controllers/Regisztracio.java
+++ Project_files/src/main/java/Controllers/Regisztracio.java
@@ -1,12 +1,17 @@
 package Controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Regisztracio {
+
+    @FXML
+    Button registButt;
 
     public void regButtonClicked() throws Exception{
         Parent newUser = FXMLLoader.load(getClass().getResource("/FXML/Udvozlo.fxml"));
@@ -15,6 +20,8 @@ public class Regisztracio {
         regStage.getIcons().add(new Image("/Pictures/Icon.png"));
         regStage.setScene(new Scene(newUser, 600, 400));
         regStage.show();
+        Stage stage = (Stage) registButt.getScene().getWindow();
+        stage.close();
     }
 
 }
--- Project_files/src/main/java/Controllers/Udvozlo.java
+++ Project_files/src/main/java/Controllers/Udvozlo.java
@@ -1,12 +1,16 @@
 package Controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Udvozlo {
+    @FXML
+    Button regButt;
 
     public void regButtonClicked() throws Exception {
         Parent newUser = FXMLLoader.load(getClass().getResource("/FXML/Regisztracio.fxml"));
@@ -15,6 +19,8 @@ public class Udvozlo {
         regStage.getIcons().add(new Image("/Pictures/Icon.png"));
         regStage.setScene(new Scene(newUser, 600, 400));
         regStage.show();
+        Stage stage = (Stage) regButt.getScene().getWindow();
+        stage.close();
 
     }
 }
