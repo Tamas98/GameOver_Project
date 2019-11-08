f61592a798fbf63affa685293bf23554cf81755a
TothCsaba <99letter86@gmail.com>
2019-11-08 23:38:38 +0100

    Resztració UI - warningok kitöltetlen helyek esetén

--- Project_files/src/main/java/Controllers/Regisztracio.java
+++ Project_files/src/main/java/Controllers/Regisztracio.java
@@ -1,10 +1,14 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
@@ -12,8 +16,30 @@ public class Regisztracio {
 
     @FXML
     Button registButt;
+    @FXML
+    TextField registName;
+    @FXML
+    PasswordField registPasw;
+    @FXML
+    Label warningName, warningPassw;
 
-    public void regButtonClicked() throws Exception{
+    public void registCheck() throws Exception{
+        warningName.setVisible(false);
+        warningPassw.setVisible(false);
+        if(registName.getText().equals("") && registPasw.getText().equals("")){
+                warningName.setVisible(true);
+                warningPassw.setVisible(true);
+
+        }else if(registName.getText().equals("")){
+           warningName.setVisible(true);
+        }else if(registPasw.getText().equals("")){
+            warningPassw.setVisible(true);
+        }else {
+            backToLogin();
+        }
+    }
+
+    public void backToLogin() throws Exception{
         Parent newUser = FXMLLoader.load(getClass().getResource("/FXML/Udvozlo.fxml"));
         Stage regStage = new Stage();
         regStage.setTitle("Game_Over");
