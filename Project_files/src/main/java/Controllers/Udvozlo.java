0ab8a68d8a38c750479bce4a2e9661c55cc224be
kristoferdei <erdeikristof@t-online.hu>
2019-11-11 16:42:15 +0100

    Fokepernyo.fxml - elkezdése

--- Project_files/src/main/java/Controllers/Udvozlo.java
+++ Project_files/src/main/java/Controllers/Udvozlo.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 
 public class Udvozlo {
     @FXML
-    Button regButt,newPassButt;
+    Button regButt,newPassButt, signInButt;
 
     public void newPasswButtonClicked() throws Exception {
         Parent newUser = FXMLLoader.load(getClass().getResource("/FXML/Uj_jelszo.fxml"));
@@ -34,5 +34,16 @@ public class Udvozlo {
         Stage stage = (Stage) regButt.getScene().getWindow();
         stage.close();
 
+    }
+
+    public void signInButtClicked() throws Exception {
+        Parent newUser = FXMLLoader.load(getClass().getResource("/FXML/Fokepernyo.fxml"));
+        Stage mainStage = new Stage();
+        mainStage.setTitle("Game_Over");
+        mainStage.getIcons().add(new Image("/Pictures/Icon.png"));
+        mainStage.setScene(new Scene(newUser, 600, 400));
+        mainStage.show();
+        Stage stage = (Stage) signInButt.getScene().getWindow();
+        stage.close();
     }
 }
