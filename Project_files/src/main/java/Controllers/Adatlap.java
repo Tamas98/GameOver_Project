e26ae4f5d120a4f302dc6a4ea4b76d6585f08a4c
TothCsaba <99letter86@gmail.com>
2019-11-25 17:13:57 +0100

    'Sikeres mentés' jelzése ha lementettük a játékot

--- Project_files/src/main/java/Controllers/Adatlap.java
+++ Project_files/src/main/java/Controllers/Adatlap.java
@@ -1,19 +1,31 @@
 package Controllers;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URI;
+import java.sql.Time;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
 
 public class Adatlap {
 
     @FXML
     Button backButton, trailerButton;
+
+    @FXML
+    Label saveSuc;
 
     /**
      * Vissza gombra való klikkelés,
@@ -40,6 +52,10 @@ public class Adatlap {
     public void trailerButtonClicked() throws Exception {
         Desktop browser = Desktop.getDesktop();
         browser.browse(new URI("https://youtu.be/6wlvYh0h63k"));
+    }
+
+    public void saveButtonClicked(){
+        saveSuc.setVisible(true);
     }
 
 }
